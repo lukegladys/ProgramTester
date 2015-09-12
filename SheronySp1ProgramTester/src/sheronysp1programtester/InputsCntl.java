1a5ec5387fc22a7dfc1d4efb4fd8d0b7ceb7b7b7
nsherony <n.sherony@gmail.com>
2015-09-12 08:35:58 -0400

    Nia's Pieces Added
    The flow generally goes:
    
    1. SheronySp1... houses the main method which
    2. Opens the InputsCntl (serving as the controller) that creates several
    Runs (model object) which should be created by Dana's part in the real
    project. Then
    3. The interface (ScannerInputs) is opened by the controller. Within the
    inputs, the code for the submit button  checks to see if it's the last
    "run" in the runlist If not, it saves the info the user inputs as a new
    run and opens a new, identical dialog for the next "run".
    4. If it is the last run, the view triggers the print method in the
    controller.

---
+++ SheronySp1ProgramTester/src/sheronysp1programtester/InputsCntl.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sheronysp1programtester;
+
+/**
+ *
+ * @author Nia
+ */
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class InputsCntl {    
+    
+    public InputsCntl(){
+        Run newRun1 = new Run ("Factorial");
+        Run newRun2 = new Run ("Factorial");
+        Run newRun3 = new Run ("ArraySearcher");
+        Run newRun4 = new Run ("ArraySearcher");
+        Run newRun5 = new Run ("TestProgram");
+        int counter = 0;
+        ScannerInputs newScannerInput = new ScannerInputs(Run.getRunList().get(counter).getFileName(), counter, Run.getRunList().size(), this);
+    }
+    
+    public void printFiles(){
+        try{
+            PrintWriter pw = new PrintWriter(new FileWriter("args.txt"));
+
+            for(int i = 0; i < Run.getRunList().size(); i++){
+                pw.println(Run.getRunList().get(i).getFileName() + " " + Run.getRunList().get(i).getCmdArgs());
+            }
+
+            pw.close();
+
+            PrintWriter pwt = new PrintWriter(new FileWriter("TestInput.txt"));
+
+            for(int i = 0; i < Run.getRunList().size(); i++){
+                pwt.println(Run.getRunList().get(i).getScannerArgs());
+            }
+
+            pwt.close();
+        }catch(IOException ex)
+            {
+            ex.printStackTrace();
+            }
+    }
+    
+    
+}
---
+++ SheronySp1ProgramTester/src/sheronysp1programtester/SheronySp1ProgramTester.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sheronysp1programtester;
+
+/**
+ *
+ * @author Nia
+ */
+public class SheronySp1ProgramTester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InputsCntl theInputsCntl = new InputsCntl();
+    }
+    
+}
