bf270c51f0dd6d45468f00da3a0db5d6dfc35457
dqc5226 <dqc5226@202-NWD28.up.ist.local>
2015-10-01 20:50:30 -0400

    Added functionality and mockup UI for step 3. This code allows the user to input up to two filenames they want to run and select a number of runs for each (up to 5). It creates an ArrayList of Run objects (created by Nia). For example, if the user wants to run Factorial twice and ArrayTest three times, it returns an ArrayList of 5 Run objects: Factorial, Factorial, ArrayTest, ArrayTest, ArrayTest. That list then goes on to the next step, where the user sets the command line arguments for each. "Connecting" step still needed: passing the ArrayList from step 3 to step 4. Currently, there is a hard-coded list of Run objects in Run.java.

---
+++ ProgramTester/src/programtester/DanaMockup.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programtester;
+
+import programtester.models.DanaMockupModel;
+
+/**
+ *
+ * @author dqc5226
+ */
+public class DanaMockup 
+{
+    public static void main(String[] args)
+    {
+        DanaMockupModel theThing = new DanaMockupModel();
+    }
+}
